e87a88b34a1f7bc0c78587e0129a977901d2e226
alon muroch <alonmuroch@gmail.com>
2017-02-21 12:15:29 +0200

    added rsk netowrk

--- truffle.js
+++ truffle.js
@@ -4,6 +4,11 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
-    }
+    },
+  	RSK: {
+  	  host: "139.162.194.77",
+      port: 4444,
+      network_id: "*" // Match any network id
+  	}
   }
 };
