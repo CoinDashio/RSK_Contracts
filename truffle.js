508d71894927e5254250a735a610bddd1b476249
Alon Muroch <alonmuroch@gmail.com>
2017-02-22 07:25:37 +0000

    deploys to RSK

--- truffle.js
+++ truffle.js
@@ -8,7 +8,10 @@ module.exports = {
   	RSK: {
   	  host: "139.162.194.77",
       port: 4444,
-      network_id: "*" // Match any network id
+      network_id: 1,        // Ethereum public network
+      from: '0xe567ad3ea4c75a647cc1bd1f0d771f4f3e19c60c',
+      gasPrice: 1000,
+      gas: 3000000
   	}
   }
 };
