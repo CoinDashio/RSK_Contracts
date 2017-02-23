666bc7d2fc20eb0ade07bce9dc44d857c3553301
Alon Muroch <alonmuroch@gmail.com>
2017-02-23 17:20:01 +0000

    updated from address for deployment

--- truffle.js
+++ truffle.js
@@ -9,7 +9,7 @@ module.exports = {
   	  host: "139.162.194.77",
       port: 4444,
       network_id: 1,        // Ethereum public network
-      from: '0xe567ad3ea4c75a647cc1bd1f0d771f4f3e19c60c',
+      from: '0x1efe06005917cd3ec6a018958bd314e1655efefb',
       gasPrice: 1000,
       gas: 3000000
   	}
